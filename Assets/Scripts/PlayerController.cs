ddc9d463ed7bc5373dfc5c759565c80651c3cbbe
Shane D. Brewer <sdb137@pitt.edu>
2026-03-12 23:08:21 -0400

    Feat: Implement player animation logic in PlayerController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,8 +4,11 @@ public class PlayerController : MonoBehaviour
 {
 	public float speed = 4.0f;
 
+	private Animator animator;
+
 	// Start is called once before the first execution of Update after the MonoBehaviour is created
 	void Start() {
+		animator = GetComponent<Animator>();
 	}
 
 	// Update is called once per frame
@@ -17,24 +20,35 @@ public class PlayerController : MonoBehaviour
 	void MovePlayer() {
 		float moveX = 0f;
 		float moveY = 0f;
+
+		bool isRunning = false;
+		bool isRunningBackward = false;
 		
 		// get direction based on input
 		if(Input.GetKey(KeyCode.RightArrow)) {
 			moveX = 1f;
-		}
+			isRunning = true;
+        }
 		if(Input.GetKey(KeyCode.LeftArrow)) {
 			moveX = -1f;
-		}
+			isRunning = true;
+        }
 		if(Input.GetKey(KeyCode.UpArrow)) {
 			moveY = 1f;
-		}
+            isRunning = true;
+            isRunningBackward = true;
+        }
 		if(Input.GetKey(KeyCode.DownArrow)) {
 			moveY = -1f;
+			isRunning = true;
 		}
-		
+
+		//set animator state once, reset when no keys are held
+		animator.SetBool("isRunning", isRunning);
+		animator.SetBool("isRunningBackward", isRunningBackward);
+
 		// create vector
 		Vector2 movement = new Vector2(moveX, moveY);
-		
 		// move character
 		transform.Translate(movement * speed * Time.deltaTime);
 	}
