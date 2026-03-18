ee13fd2bc8c5b39b52088ab36b4150ecdcc14a29
Shane D. Brewer <sdb137@pitt.edu>
2026-03-18 15:02:16 -0400

    Refactor: Refine Player Controller debugging

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,7 +13,7 @@ public class PlayerController : MonoBehaviour
 
 	// Update is called once per frame
 	void Update() {
-		Debug.Log("current pos " + transform.position);
+		//Debug.Log("current pos " + transform.position);
 		MovePlayer();
 	}
 
